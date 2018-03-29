d6e9206b3496a9678b6b7cd63e246104c50bdd41
Michael Greve <michael@grevens.dk>
2018-03-29 15:35:42 +0200

    Initial setup for Material2 and Roboto done

---
+++ src/app/material.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+const materialModules = [];
+
+@NgModule({
+    imports: [
+        BrowserAnimationsModule,
+        ...materialModules
+    ],
+    exports: [
+        ...materialModules
+    ]
+})
+
+export class MaterialModule {
+}
